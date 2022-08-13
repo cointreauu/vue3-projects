a625d8a5c7056119cc246e8a3b145a4af3ada3a0
wonkyung yun <uni0709@naver.com>
2022-08-13 22:32:11 +0900

    v-show, v-for, v-if

--- src/App.vue
+++ src/App.vue
@@ -2,51 +2,77 @@
   <div class="container">
     <h2>To-do List</h2>
     <form
-      class="d-flex"
       @submit.prevent="onSubmit"
     >
-      <div class="flex-grow-1 mr-2">
-        <input
-          v-model="todo"
-          class="form-control"
-          type="text"
-          placeholder="Type new to-do"
-        >
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+          <input
+            v-model="todo"
+            class="form-control"
+            type="text"
+            placeholder="Type new to-do"
+          >
+        </div>
+        <div>
+          <button 
+            class="btn btn-primary"
+            type="submit"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div>
-        <button 
-          class="btn btn-primary"
-          type="submit"
-        >
-          Add
-        </button>
+
+      <div
+        v-show="hasError"
+        style="color: red"
+      >
+        This field cannot be empty.
       </div>
     </form>
-    {{ todoList }}
+    <div 
+      v-for="item in todoList"
+      :key="item.id"
+      class="card mt-2"
+    >
+      <div class="card-body p-2">
+        {{ item.subject }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import {ref} from 'vue';    // import
+import {ref} from 'vue';
 
 export default{
   setup(){
+    const hasError = ref(false);
     const todo = ref('');
-    const todoList = ref([]);
+    const todoList = ref([
+      {id: 1, subject: '휴대폰 사기'},
+      {id: 2, subject: '장보기'},
+    ]);
 
     const onSubmit = () => {
-      //e.preventDefault();   // 자동 refresh 방지
-      todoList.value.push({
+      if (todo.value === ''){
+        hasError.value = true;
+      } else {
+        todoList.value.push({
         id: Date.now(),
         subject: todo.value
       });
+        hasError.value=false;
+        todo.value = '';
+      }      
     };
 
     return {
       todo,
       todoList,
-      onSubmit
-    }
+      onSubmit,
+      hasError,
+    };
   }
 }
 </script>
